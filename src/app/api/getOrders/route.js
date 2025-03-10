95aa182df380c06bdcae9c45d02e5ba4b33a9ecc
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-10 14:39:07 +0500

    order-summary

--- src/app/api/getOrders/route.js
+++ src/app/api/getOrders/route.js
@@ -1,8 +1,8 @@
-import dbConnection from '@/config/dbConnection';
-import ProductModel from '@/models/productModel';
-import { NextResponse } from 'next/server';
+import dbConnection from "@/config/dbConnection";
+import ordersModel from "@/models/orderModel";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'; // Opt out of static rendering
+export const dynamic = "force-dynamic"; // Opt out of static rendering
 
 export async function GET(request) {
   try {
@@ -10,25 +10,20 @@ export async function GET(request) {
 
     // Extract query parameters from the URL
     const { searchParams } = new URL(request.url);
-    const orderId = searchParams.get('order_id');
+    const orderId = searchParams.get("order_id");
 
     // If product_id is provided, fetch a single product by ID
-      const order = await ProductModel.findById(orderId);
+    const order = await ordersModel.findById(orderId);
 
-      if (!order) {
-        return NextResponse.json(
-          { error: 'Order not found' },
-          { status: 404 }
-        );
-      }
-    
+    if (!order) {
+      return NextResponse.json({ error: "Order not found" }, { status: 404 });
+    }
+
     return NextResponse.json(order);
-
-    
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: 'Internal Server Error' },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
