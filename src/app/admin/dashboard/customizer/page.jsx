ceab41f1c612e9abb1d469e71533163a19708cd5
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-24 12:36:50 +0500

    use-state

--- src/app/admin/dashboard/customizer/page.jsx
+++ src/app/admin/dashboard/customizer/page.jsx
@@ -1,40 +1,41 @@
-"use client"
+"use client";
 
-import React from 'react'
-import {Tabs, Tab, Card, CardBody} from "@heroui/react";
-import Hoodie from './Hoodie';
-import Shirt from './Shirt';
-import Zipper from './Zipper';
-
+import React, { useState } from "react";
+import { Tabs, Tab, Card, CardBody, Checkbox } from "@heroui/react";
+import Hoodie from "./Hoodie";
+import Shirt from "./Shirt";
+import Zipper from "./Zipper";
 
 const Customizer = () => {
+  const [isSelected, setIsSelected] = React.useState(false);
+
   return (
     <div>
-
+      {/* checkbox */}
+      <div className="flex flex-col gap-2">
+        <Checkbox isSelected={isSelected} onValueChange={setIsSelected}>
+          Customize
+        </Checkbox>
+        <p className="text-default-500">
+          Selected: {isSelected ? "true" : "false"}
+        </p>
+      </div>
+      {/* Other details */}
       <div className="flex w-full flex-col">
         <Tabs aria-label="Options">
           <Tab key="photos" title="Hoodie">
-            
             <Hoodie />
-            
           </Tab>
           <Tab key="music" title="Shirt">
-            
             <Shirt />
-
           </Tab>
           <Tab key="videos" title="Zipper">
-            
             <Zipper />
-            
           </Tab>
         </Tabs>
       </div>
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Customizer
+export default Customizer;
